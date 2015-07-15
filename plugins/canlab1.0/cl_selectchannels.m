5e2f4225fbbe0dd64972c1d371369f13b1d832c8
Jorge Yanar <jorgeyanar@gmail.com>
2015-07-15 12:46:55 -0500

    Channel selection -- added clinical channels

--- plugins/canlab1.0/cl_selectchannels.m
+++ plugins/canlab1.0/cl_selectchannels.m
@@ -1,6 +1,7 @@
 % This script allows the user to select channels to average, exclude, or
 % include in a number of datasets prior to running analysis. 
-
+% Clinical Electrodes:
+% 
 importpath = uigetdir('~', 'Select directory to import from');
 exportpath = uigetdir('~', 'Select directory to export to');
 if importpath == 0
@@ -11,13 +12,16 @@ files = dir(fullfile(strcat(importpath, '/*.set')));
 %   TODO: Create GUI that allows user to easily select bad channels
 %disp('Select the channels that you would like to exclude: ');
 %disp(EEG.chaninfo.filecontent);
-chans = [1, 2];
+clinicalCh = [3 4 5 9 11 12 13 14 17 18 19 20 21 22 23 24 25 28 30 32 34 35 37 38 39 41 42 43 44 45 47 48 49 50 51 52 53 54 57 59 61 63 64 65 66];
 for id = 1:numel(files)
+    EEG = pop_loadset(files(id).name, importpath);
     % Now we remove selected data channels from all of the datasets.
     % pop_select removes the channels from the actual data, whereas
     % pop_chanedit removes the channels from the EEG struct
-    fprintf('Removing the following channels from %s:\n', files(id).name);
-    disp(EEG.chaninfo.filecontent(chans, :));
-    EEG = pop_select(EEG, 'nochannel', chans);
-    EEG = pop_chanedit(EEG, 'delete', chans);
+    for ChId = 1:numel(clinicalCh)
+        EEG.chanlocs(clinicalCh(ChId)) = [];
+        EEG.data(clinicalCh(ChId)) = [];
+    end
+    pop_saveset(EEG, 'filename', files(id).name(1:end-4), 'filepath',...
+        exportpath, 'savemode', 'onefile');
 end
