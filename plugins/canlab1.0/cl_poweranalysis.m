a582302efe67ac437333f9601faac67be4d2a0d8
Jorge Yanar <jorgeyanar@gmail.com>
2015-07-16 13:09:54 -0500

    Actually, calculating mean power in fixed bands takes a few seconds. cl_poweranalysis now calculates power in both fixed and calculated bands by default.

--- plugins/canlab1.0/cl_poweranalysis.m
+++ plugins/canlab1.0/cl_poweranalysis.m
@@ -10,9 +10,7 @@ exportpath = uigetdir('C:\Users\canlab\Documents\MATLAB\', 'Select folder to exp
 if exportpath == 0
     error('Error: Please specify the folder to export results files to.');
 end
-% Change this to 0 in order to compute the IAF, TF, and calculate power
-% from the resulting frequency bands.
-fixedBands = 1;
+
 files = dir(fullfile(strcat(importpath, '/*S.mat')));
 subj{size(files, 1)} = [];
 
@@ -20,11 +18,10 @@ for i = 1:numel(files)
     [Signal, SignalInfo, path] = nbt_load_file(strcat(importpath, '/', files(i).name));
     [power, freq] = spectopo(mean(Signal'), 0, 512, 'plot', 'off');
     subj{i}.SubjectID = files(i).name;
-    subj{i}.fixedBands = fixedBands;
     subj{i}.power = power;
     subj{i}.freq = freq;
-    % Because we are using fixed frequency bands, fitting peaks is not
-    % necessary, and we can simply define the indexes:
+    
+    % ---- FIXED FREQUENCY BANDS ---- %
     subj{i}.fixedTheta_floor    = 4;
     subj{i}.fixedTheta_ceiling  = 8;
     subj{i}.fixedAlpha_floor    = 8;
@@ -37,39 +34,34 @@ for i = 1:numel(files)
     subj{i}.fixedAlpha_ceiling  = 13;
     subj{i}.fixedBeta_floor     = 13;
     subj{i}.fixedBeta_ceiling   = 30;
-        
-    if fixedBands == 1
-        analysis = 'fixed';
-        % Calculate power across fixed (traditional) frequency bands
-        subj{i}.mean_falphaPower  = mean(power(freq >= subj{i}.fixedAlpha_floor & freq <= subj{i}.fixedAlpha_ceiling));
-        subj{i}.mean_fthetaPower  = mean(power(freq >= subj{i}.fixedTheta_floor & freq <= subj{i}.fixedTheta_ceiling));
-        subj{i}.mean_falpha1Power = mean(power(freq >= subj{i}.fixedAlpha1_floor & freq <= subj{i}.fixedAlpha1_ceiling));
-        subj{i}.mean_falpha2Power = mean(power(freq >= subj{i}.fixedAlpha2_floor & freq <= subj{i}.fixedAlpha2_ceiling));
-        subj{i}.mean_falpha3Power = mean(power(freq >= subj{i}.fixedAlpha3_floor & freq <= subj{i}.fixedAlpha3_ceiling));
-        % Compute ratios and store Power Spectrum
-        subj{i}.ratio_meanAlpha32Fixed    = subj{i}.mean_falpha3Power / subj{i}.mean_falpha2Power;
-        subj{i}.ratio_meanAlphaThetaFixed = subj{i}.mean_falphaPower / subj{i}.mean_fthetaPower;
-    else
-        analysis = 'IAF';
-        % Calculate power based on bands derived from IAF and TF
-        subj{i}.peakFit = nbt_doPeakFit(Signal, SignalInfo);
-        % Find mean IAF, TF, and find needed indexes
-        disp('Peak Fit complete, calculating indexes');    
-        subj{i}.meanIAF = mean(subj{i}.peakFit.IAF);
-        subj{i}.meanTF  = mean(subj{i}.peakFit.TF);
-        subj{i}.Theta_floor    = subj{i}.meanTF - 2;
-        subj{i}.Alpha2_floor   = (subj{i}.meanIAF + subj{i}.meanTF) / 2;
-        subj{i}.Alpha3_ceiling = subj{i}.meanIAF + 2;
-        % Compute power across derived bands
-        subj{i}.mean_thetaPower = mean(power(freq >= subj{i}.Theta_floor & freq <= subj{i}.meanTF));
-        subj{i}.mean_alphaPower = mean(power(freq >= subj{i}.TF & freq <= subj{i}.Alpha3_ceiling));
-        subj{i}.mean_alpha1Power = mean(power(freq >= subj{i}.meanTF & freq <= subj{i}.Alpha2_floor));
-        subj{i}.mean_alpha2Power = mean(power(freq >= subj{i}.Alpha2_floor & freq <= subj{i}.meanIAF));
-        subj{i}.mean_alpha3Power = mean(power(freq >= subj{i}.meanIAF & freq <= subj{i}.Alpha3_ceiling));
-        subj{i}.ratio_meanAlpha32 = subj{i}.mean_alpha3Power / subj{i}.mean_alpha2Power;
-        subj{i}.ratio_meanAlphaTheta = subj{i}.mean_alphaPower / mean_thetaPower;
-    end
+    % Calculate power across fixed (traditional) frequency bands
+    subj{i}.mean_falphaPower  = mean(power(freq >= subj{i}.fixedAlpha_floor & freq <= subj{i}.fixedAlpha_ceiling));
+    subj{i}.mean_fthetaPower  = mean(power(freq >= subj{i}.fixedTheta_floor & freq <= subj{i}.fixedTheta_ceiling));
+    subj{i}.mean_falpha1Power = mean(power(freq >= subj{i}.fixedAlpha1_floor & freq <= subj{i}.fixedAlpha1_ceiling));
+    subj{i}.mean_falpha2Power = mean(power(freq >= subj{i}.fixedAlpha2_floor & freq <= subj{i}.fixedAlpha2_ceiling));
+    subj{i}.mean_falpha3Power = mean(power(freq >= subj{i}.fixedAlpha3_floor & freq <= subj{i}.fixedAlpha3_ceiling));
+    % Compute ratios
+    subj{i}.ratio_meanAlpha32Fixed    = subj{i}.mean_falpha3Power / subj{i}.mean_falpha2Power;
+    subj{i}.ratio_meanAlphaThetaFixed = subj{i}.mean_falphaPower / subj{i}.mean_fthetaPower;
+    
+    % ---- FREQUENCY BANDS DERIVED FROM IAF & TF ---- %
+    subj{i}.peakFit = nbt_doPeakFit(Signal, SignalInfo);
+    % Find mean IAF, TF, and find needed indexes 
+    subj{i}.meanIAF = mean(subj{i}.peakFit.IAF);
+    subj{i}.meanTF  = mean(subj{i}.peakFit.TF);
+    subj{i}.Theta_floor    = subj{i}.meanTF - 2;
+    subj{i}.Alpha2_floor   = (subj{i}.meanIAF + subj{i}.meanTF) / 2;
+    subj{i}.Alpha3_ceiling = subj{i}.meanIAF + 2;
+    % Compute power across derived bands
+    subj{i}.mean_thetaPower  = mean(power(freq >= subj{i}.Theta_floor & freq <= subj{i}.meanTF));
+    subj{i}.mean_alphaPower  = mean(power(freq >= subj{i}.TF & freq <= subj{i}.Alpha3_ceiling));
+    subj{i}.mean_alpha1Power = mean(power(freq >= subj{i}.meanTF & freq <= subj{i}.Alpha2_floor));
+    subj{i}.mean_alpha2Power = mean(power(freq >= subj{i}.Alpha2_floor & freq <= subj{i}.meanIAF));
+    subj{i}.mean_alpha3Power = mean(power(freq >= subj{i}.meanIAF & freq <= subj{i}.Alpha3_ceiling));
+    % Compute ratios
+    subj{i}.ratio_meanAlpha32    = subj{i}.mean_alpha3Power / subj{i}.mean_alpha2Power;
+    subj{i}.ratio_meanAlphaTheta = subj{i}.mean_alphaPower / mean_thetaPower;
 end
-name = strcat(date, '-results-', analysis, '.mat');
+name = strcat(date, '-results', '.mat');
 save(name, 'subj');
 
