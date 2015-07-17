22f2221526c08052a42217bb76b09721c58700b5
Jorge Yanar <jorgeyanar@gmail.com>
2015-07-17 12:53:30 -0500

    Added extended clinical montage, and in process of changing approach to power analysis. This approach will feature different averaging process, and for alpha/theta ratio: use only O1, C3 electrodes.

--- plugins/canlab1.0/cl_poweranalysis.m
+++ plugins/canlab1.0/cl_poweranalysis.m
@@ -1,5 +1,8 @@
-% This script utilizes nbt_doPeakFit and EEGLAB's spectopo() function to calculate the mean
-% power ratio for power bands derived from calculating the TF and IAF. 
+% This script utilizes nbt_doPeakFit and EEGLAB's spectopo() function to calculate
+% absolute power across both calculated power bands and fixed ones. Note
+% that since spectopo() returns the Power Spectrum Density in
+% 10*log10(uV^2), we need to apply a few transformations to acquire uV^2,
+% or absolute power. 
 
 importpath = uigetdir('~', 'Select folder to import from (contains .mat files)');
 if importpath == 0
@@ -11,15 +14,31 @@ if exportpath == 0
     error('Error: Please specify the folder to export results files to.');
 end
 fprintf('Export path: %s\n', exportpath);
-
+cd ~/nbt
+installNBT;
 files = dir(fullfile(strcat(importpath, '/*S.mat')));
 subj{size(files, 1)} = [];
+
 for i = 1:numel(files)
     [Signal, SignalInfo, path] = nbt_load_file(strcat(importpath, '/', files(i).name));
-    [spectra, freq] = spectopo(mean(Signal'), 0, 512, 'plot', 'off', 'freqrange', [0 45]);
     subj{i}.SubjectID = files(i).name;
-    subj{i}.spectra = spectra; % Note that units are 10*log10(amp^2)
-    subj{i}.freq = freq;
+    subj{i}.IAFs      = [];
+    for j = 1:size(Signal,2)
+        % Calculate IAF, TF for each channel, and then find the average for
+        % the IAF and TF, excluding the NaN values. Check: Does computing
+        % IAF, TF for each channel produce the same value as taking the
+        % grand average and computing the IAF / TF?
+        tempPeakObj  = nbt_doPeakFit(Signal(:,j), SignalInfo);
+        subj{i}.IAFs = [subj{i}.IAFs; tempPeakObj.IAF];
+        subj{i}.TFs  = [subj{i}.TFs; tempPeakObj.TF];
+        % TODO: Save C3, O1 electrode IAFs and TFs
+    end
+    subj{i}.meanIAF = nanmean(subj{i}.IAFs);
+    subj{i}.meanTF  = nanmean(subj{i}.TFs);
+    % Take the grand average for the subject, then find PSD of grand average
+    [avgPSD, avgFreq] = spectopo(mean(Signal'), 0, 512, 'plot', 'off', 'avgFreqrange', [0 45]);
+    subj{i}.avgPSD  = avgPSD;
+    subj{i}.avgFreq = avgFreq;
     
     % ---- FIXED FREQUENCY BANDS ---- %
     subj{i}.fixedDelta_floor    = 0;
@@ -37,18 +56,8 @@ for i = 1:numel(files)
     subj{i}.fixedBeta_floor     = 13;
     subj{i}.fixedBeta_ceiling   = 30;
     subj{i}.fixedGamma_floor    = 30;
-    subj{i}.Gamma_ceiling       = 45; % Gamma ceiling is 45 Hz for fixed and IAF-based bands
+    subj{i}.fixedGamma_ceiling  = 45; % Gamma ceiling is 45 Hz for fixed and IAF-based bands
     
-    % Calculate absolute power (amplitude^2) across fixed (traditional) frequency bands
-    subj{i}.mean_fdeltaPower  = mean(10.^(spectra(freq >= subj{i}.fixedDelta_floor & freq <= subj{i}.fixedDelta_ceiling)/10));
-    subj{i}.mean_fthetaPower  = mean(10.^(spectra(freq >= subj{i}.fixedTheta_floor & freq <= subj{i}.fixedTheta_ceiling)/10));
-    subj{i}.mean_falphaPower  = mean(10.^(spectra(freq >= subj{i}.fixedAlpha_floor & freq <= subj{i}.fixedAlpha_ceiling)/10));
-    subj{i}.mean_fbetaPower   = mean(10.^(spectra(freq >= subj{i}.fixedBeta_floor & freq <= subj{i}.fixedBeta_ceiling)/10));
-    subj{i}.mean_fgammaPower  = mean(10.^(spectra(freq >= subj{i}.fixedGamma_floor & freq <= subj{i}.Gamma_ceiling)/10));
-    subj{i}.mean_falpha1Power = mean(10.^(spectra(freq >= subj{i}.fixedAlpha1_floor & freq <= subj{i}.fixedAlpha1_ceiling)/10));
-    subj{i}.mean_falpha2Power = mean(10.^(spectra(freq >= subj{i}.fixedAlpha2_floor & freq <= subj{i}.fixedAlpha2_ceiling)/10));
-    subj{i}.mean_falpha3Power = mean(10.^(spectra(freq >= subj{i}.fixedAlpha3_floor & freq <= subj{i}.fixedAlpha3_ceiling)/10));
-
     % ---- FREQUENCY BANDS DERIVED FROM IAF & TF ---- %
     subj{i}.peakFit = nbt_doPeakFit(Signal, SignalInfo);
     subj{i}.meanIAF = mean(subj{i}.peakFit.IAF);
@@ -84,19 +93,37 @@ for i = 1:numel(files)
     %subj{i}.Gamma_floor    = 
     % -- Gamma ceiling already set
     
-    % Compute power across derived bands
-    subj{i}.mean_DeltaPower  = mean(10.^(spectra(freq >= subj{i}.Delta_floor & freq <= subj{i}.Delta_ceiling)/10));
-    subj{i}.mean_thetaPower  = mean(10.^(spectra(freq >= subj{i}.Theta_floor & freq <= subj{i}.Theta_ceiling)/10));
-    subj{i}.mean_alphaPower  = mean(10.^(spectra(freq >= subj{i}.Alpha_floor & freq <= subj{i}.Alpha_ceiling)/10));
-    subj{i}.mean_alpha1Power = mean(10.^(spectra(freq >= subj{i}.meanTF & freq <= subj{i}.Alpha2_floor)/10));
-    subj{i}.mean_alpha2Power = mean(10.^(spectra(freq >= subj{i}.Alpha2_floor & freq <= subj{i}.meanIAF)/10));
-    subj{i}.mean_alpha3Power = mean(10.^(spectra(freq >= subj{i}.meanIAF & freq <= subj{i}.Alpha3_ceiling)/10));
+    % Calculate absolute power (amplitude^2) across fixed (traditional) frequency bands
+    subj{i}.mean_fdeltaPower  = mean(10.^(avgPSD(avgFreq >= subj{i}.fixedDelta_floor  & avgFreq <= subj{i}.fixedDelta_ceiling)/10));
+    subj{i}.mean_fthetaPower  = mean(10.^(avgPSD(avgFreq >= subj{i}.fixedTheta_floor  & avgFreq <= subj{i}.fixedTheta_ceiling)/10));
+    subj{i}.mean_falphaPower  = mean(10.^(avgPSD(avgFreq >= subj{i}.fixedAlpha_floor  & avgFreq <= subj{i}.fixedAlpha_ceiling)/10));
+    subj{i}.mean_fbetaPower   = mean(10.^(avgPSD(avgFreq >= subj{i}.fixedBeta_floor   & avgFreq <= subj{i}.fixedBeta_ceiling)/10));
+    subj{i}.mean_fgammaPower  = mean(10.^(avgPSD(avgFreq >= subj{i}.fixedGamma_floor  & avgFreq <= subj{i}.Gamma_ceiling)/10));
+    subj{i}.mean_falpha1Power = mean(10.^(avgPSD(avgFreq >= subj{i}.fixedAlpha1_floor & avgFreq <= subj{i}.fixedAlpha1_ceiling)/10));
+    subj{i}.mean_falpha2Power = mean(10.^(avgPSD(avgFreq >= subj{i}.fixedAlpha2_floor & avgFreq <= subj{i}.fixedAlpha2_ceiling)/10));
+    subj{i}.mean_falpha3Power = mean(10.^(avgPSD(avgFreq >= subj{i}.fixedAlpha3_floor & avgFreq <= subj{i}.fixedAlpha3_ceiling)/10));
+
+    % Compute absolute power across derived bands
+    subj{i}.mean_DeltaPower  = mean(10.^(avgPSD(avgFreq >= subj{i}.Delta_floor  & avgFreq <= subj{i}.Delta_ceiling)/10));
+    subj{i}.mean_thetaPower  = mean(10.^(avgPSD(avgFreq >= subj{i}.Theta_floor  & avgFreq <= subj{i}.Theta_ceiling)/10));
+    subj{i}.mean_alphaPower  = mean(10.^(avgPSD(avgFreq >= subj{i}.Alpha_floor  & avgFreq <= subj{i}.Alpha_ceiling)/10));
+    subj{i}.mean_alpha1Power = mean(10.^(avgPSD(avgFreq >= subj{i}.meanTF       & avgFreq <= subj{i}.Alpha2_floor)/10));
+    subj{i}.mean_alpha2Power = mean(10.^(avgPSD(avgFreq >= subj{i}.Alpha2_floor & avgFreq <= subj{i}.meanIAF)/10));
+    subj{i}.mean_alpha3Power = mean(10.^(avgPSD(avgFreq >= subj{i}.meanIAF      & avgFreq <= subj{i}.Alpha3_ceiling)/10));
     
     % Compute ratios using both fixed and calculated bands
     subj{i}.ratio_meanAlpha32Fixed    = subj{i}.mean_falpha3Power / subj{i}.mean_falpha2Power;
     subj{i}.ratio_meanAlphaThetaFixed = subj{i}.mean_falphaPower / subj{i}.mean_fthetaPower;
     subj{i}.ratio_meanAlpha32    = subj{i}.mean_alpha3Power / subj{i}.mean_alpha2Power;
     subj{i}.ratio_meanAlphaTheta = subj{i}.mean_alphaPower / subj{i}.mean_thetaPower;
+    
+    % ---- ALPHA THETA RATIO CALCULATION ---- %
+    % 1. Take grand average for both C3 and O1, average the IAF of each
+    % channel that makes up C3, O1. 
+    % 2. Calculate frequency bands for C3, O1
+    % 3. Compute AlphaTheta ratio for both C3 and O1
+    
+    
 end
 structFile = strcat(exportpath, '/', date, '-results', '.mat');
 save(structFile, 'subj');
