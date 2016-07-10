971c679660ab8f193c80dd732f811beb4d78f499
Jorge Yanar <jorgeyanar@gmail.com>
2016-07-09 20:34:35 -0500

    separate functions for montages and clustering; deleted cl_study
    - Additionally, cl_clusters now supports a sparse 10-20 clusters option

---
+++ plugins/canlab1.0/cl_clusters.m
@@ -0,0 +1,66 @@
+% TODO: Documentation for this function
+
+function cl_clusters(importpath, exportpath, preset_cluster, cluster)
+
+% ------------------------------------------------- %
+% Check user input for options regarding clustering %
+% ------------------------------------------------- %
+if strcmp(preset_cluster, '10-20-dense')
+
+    cluster(1).name = 'frontal';
+    cluster(1).electrodes = {'A01','A02','A03','A04','A05','A07','A08','B01',...
+                             'B02','B03','B05','B06'};
+    cluster(2).name = 'ltemporal';
+    cluster(2).electrodes = {'A10','A11','A17','A18','A21','A22','A27'};
+    cluster(3).name = 'central';
+    cluster(3).electrodes = {'A06','A12','A13','A14','A15','A16','A23','A24',...
+                             'A25','B04','B10','B11','B12','B19','B20','B21',...
+                             'B22','B28'};
+    cluster(4).name = 'rtemporal';
+    cluster(4).electrodes = {'B08','B09','B13','B14','B17','B18','B24'};
+    cluster(5).name = 'occipital';
+    cluster(5).electrodes = {'A26','A29','A30','A31','B23','B26','B27','B29','B30'};
+
+elseif strcmp(preset_cluster, '10-20-sparse')
+
+    cluster(1).name = 'frontal';
+    cluster(1).electrodes = {'A01','A02','A07','A08','B01'};
+    cluster(2).name = 'ltemporal';
+    cluster(2).electrodes = {'A10','A27'};
+    cluster(3).name = 'central';
+    cluster(3).electrodes = {'A06','A15','A16','B04','B28'};
+    cluster(4).name = 'rtemporal';
+    cluster(4).electrodes = {'B08','B14','B24'};
+    cluster(5).name = 'occipital';
+    cluster(5).electrodes = {'A26','A29','A31','B23','B26','B30'};
+
+elseif strcmp(preset_cluster, 'custom')
+    if ~exist('cluster', 'var')
+        error('Custom clusters were not defined.');
+    end
+end
+
+files = dir(fullfile(strcat(importpath, '/*.set')));
+for i = 1:numel(files)
+
+    EEG = pop_loadset(files(i).name, importpath);
+    
+    % Add clusters to the bottom of the EEG    
+    for j = 1:numel(cluster)
+
+        % Add empty channel to the bottom of EEG.data; update EEG structure info
+        EEG.data(end+1, :) = 0;
+        EEG.nbchan = size(EEG.data, 1);
+        EEG.chanlocs(end+1).labels = cluster(j).name;
+        % Calculate mean of this cluster's electrodes
+        for ch = 1:numel(cluster(j).electrodes)
+            ch_name = cluster(j).electrodes{ch};
+            EEG.data(end, :) = EEG.data(end, :) + EEG.data(chindex(EEG.chanlocs, ch_name), :);
+        end
+        EEG.data(end, :) = EEG.data(end, :) / numel(cluster(j).electrodes);
+    end
+
+    % Save resulting EEG structure
+    pop_saveset(EEG, 'filename', files(i).name(1:end-4), 'filepath', exportpath, 'savemode', 'onefile');
+end
+end
--- plugins/canlab1.0/cl_montage.m
+++ plugins/canlab1.0/cl_montage.m
@@ -41,44 +41,19 @@
 %   >>> cl_montage('raw-set', 'excl-set', params);
 %
 
-function cl_montage(importpath, exportpath, params)
-
-% ------------------------------------------------- %
-% Check user input for options regarding clustering %
-% ------------------------------------------------- %
-if strcmp(params.preset_cluster, '10-20-dense')
-    params.cluster(1).name = 'frontal';
-    params.cluster(1).electrodes = {'A01','A02','A03','A04','A05','A07','A08','B01',...
-                                    'B02','B03','B05','B06'};
-    params.cluster(2).name = 'ltemporal';
-    params.cluster(2).electrodes = {'A10','A11','A17','A18','A21','A22','A27'};
-    params.cluster(3).name = 'central';
-    params.cluster(3).electrodes = {'A06','A12','A13','A14','A15','A16','A23','A24',...
-                                    'A25','B04','B10','B11','B12','B19','B20','B21',...
-                                    'B22','B28'};
-    params.cluster(4).name = 'rtemporal';
-    params.cluster(4).electrodes = {'B08','B09','B13','B14','B17','B18','B24'};
-    params.cluster(5).name = 'occipital';
-    params.cluster(5).electrodes = {'A26','A29','A30','A31','B23','B26','B27','B29','B30'};
-elseif strcmp(params.preset_cluster, 'custom')
-    if ~isfield(params, 'cluster')
-        error('Custom clusters were not defined.');
-    end
-else
-    params.cluster = 'none';
-end
+function cl_montage(importpath, exportpath, montage)
 
 % ---------------------------------------------- %
 % Check user input for options regarding montage %
 % ---------------------------------------------- %
-if strcmp(params.montage, 'stdClinicalCh')
+if strcmp(montage, 'stdClinicalCh')
     % Electrodes to remove, for Standard Clinical Electrodes Montage
-    params.excludedchannels = [66 65 64 63 61 59 57 54 53 52 51 50 49 48 47 45 44 43 42 41 39 38 37 35 34 32 30 28 25 24 23 22 21 20 19 18 17 14 13 12 11 9 5 4 3];
-elseif strcmp(params.montage, 'extClnicalCh')
+    excludedchannels = [66 65 64 63 61 59 57 54 53 52 51 50 49 48 47 45 44 43 42 41 39 38 37 35 34 32 30 28 25 24 23 22 21 20 19 18 17 14 13 12 11 9 5 4 3];
+elseif strcmp(montage, 'extClinicalCh')
     % Electrodes to remove, for Extended Clinical Electrodes Montage
-    params.excludedchannels = [9 19 20 28 32 39 47 48 57 63 64 65 66];
+    excludedchannels = [9 19 20 28 32 39 47 48 57 63 64 65 66];
 else % TODO: Implement ability for user to specify custom exclusion using e.g. 'A03', etc
-    params.montage = NaN;
+    error('Please specify a montage');
 end
 
 % ---------------------------------------------------------- %
@@ -86,32 +61,8 @@ end
 % ---------------------------------------------------------- %
 files = dir(fullfile(strcat(importpath, '/*.set')));
 for i = 1:numel(files)
-    
     EEG = pop_loadset(files(i).name, importpath);
-    
-    % Add clusters to the bottom of the EEG
-    if ~strcmp(params.cluster, 'none')
-        for j = 1:numel(params.cluster)
-            % Add empty channel to the bottom of EEG.data; update EEG structure info
-            EEG.data(end+1, :) = 0;
-            EEG.nbchan = size(EEG.data, 1);
-            EEG.chanlocs(end+1).labels = params.cluster(j).name;
-
-            % Calculate mean of this cluster's electrodes
-            for ch = 1:numel(params.cluster(j).electrodes)
-                ch_name = params.cluster(j).electrodes{ch};
-                EEG.data(end, :) = EEG.data(end, :) + EEG.data(chindex(EEG.chanlocs, ch_name), :);
-            end
-            EEG.data(end, :) = EEG.data(end, :) / numel(params.cluster(j).electrodes);
-        end
-    end
-
-    % Exclude specified channels
-    if ~isnan(params.montage)
-        EEG = pop_select(EEG, 'nochannel', params.excludedchannels)
-    end
-
-    % Save resulting EEG structure
+    EEG = pop_select(EEG, 'nochannel', excludedchannels);
     pop_saveset(EEG, 'filename', files(i).name(1:end-4), 'filepath', exportpath, 'savemode', 'onefile');
 end
 end
