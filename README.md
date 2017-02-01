ea446d536988dd1a18bd75f9692ea349e27ff2b8
Jorge Yanar <jorgeyanar@gmail.com>
2017-02-01 16:54:28 -0600

    channel info for dorsal, ventral src-models

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # EEGLAB
-CAN Lab's EEGLAB repository
+CAN Lab's EEGLAB repository.
 
-The plugin developed for EEGLAB is located [here](https://github.com/canlabluc/EEGLAB/tree/master/plugins/canlab1.0#can-labs-eeglab-plugin)
+The plugin developed for EEGLAB is located [here](https://github.com/canlabluc/EEGLAB/tree/master/plugins/canlab1.0#can-labs-eeglab-plugin).
--- plugins/canlab1.0/cl_importmul.m
+++ plugins/canlab1.0/cl_importmul.m
@@ -134,9 +134,105 @@ for i = 1:numel(files_set)
                   'Noise2R2','Noise2R3','NoiseF','NoiseF1','NoiseF2','NoiseF3'};
 
     elseif strcmp(montage, 'dorsal')
-        disp('asdf');
+        LFEFc  = EEG.data(1:3,   :);
+        RFEFc  = EEG.data(4:6,   :);
+        LaIPSc  = EEG.data(7:9,   :);
+        RaIPSc  = EEG.data(10:12, :);
+        LpIPSc    = EEG.data(13:15, :);
+        RpIPSc    = EEG.data(16:18, :);
+        Noise1Lc    = EEG.data(19:21, :);
+        Noise1Rc    = EEG.data(22:24, :);
+        Noise2Lc    = EEG.data(25:27, :);
+        Noise2Rc    = EEG.data(28:30, :);
+        Noise3Lc    = EEG.data(31:33, :);
+        Noise3Rc    = EEG.data(34:36, :);
+
+        LFEF = sqrt(sum(LFEFc.^2, 1));
+        RFEF = sqrt(sum(RFEFc.^2, 1));
+        LaIPS = sqrt(sum(LaIPSc.^2, 1));
+        RaIPS = sqrt(sum(RaIPSc.^2, 1));
+        LpIPS = sqrt(sum(LpIPSc.^2, 1));
+        RpIPS = sqrt(sum(RpIPSc.^2, 1));
+        Noise1L = sqrt(sum(Noise1Lc.^2, 1));
+        Noise1R = sqrt(sum(Noise1Rc.^2, 1));
+        Noise2L = sqrt(sum(Noise2Lc.^2, 1));
+        Noise2R = sqrt(sum(Noise2Rc.^2, 1));
+        Noise3L = sqrt(sum(Noise3Lc.^2, 1));
+        Noise3R = sqrt(sum(Noise3Rc.^2, 1));
+        EEG.data = [
+            LFEF  ; LFEFc;
+            RFEF  ; RFEFc;
+            LaIPS  ; LaIPSc;
+            RaIPS  ; RaIPSc;
+            LpIPS    ; LpIPSc;
+            RpIPS    ; RpIPSc;
+            Noise1L    ; Noise1Lc;
+            Noise1R    ; Noise1Rc;
+            Noise2L ; Noise2Lc;
+            Noise2R ; Noise2Rc;
+            Noise3L ; Noise3Lc;
+            Noise3R ; Noise3Rc;
+        ];
+
+        labels = {'LFEF','LFEFr','LFEFv','LFEFh','RFEF','RFEFr','RFEFv','RFEFh',...
+                  'LaIPS','LaIPSr','LaIPSv','LaIPSh','RaIPS','RaIPSr','RaIPSv',...
+                  'RaIPSh','LpIPS','LpIPSr','LpIPSv','LpIPSh','RpIPS','RpIPSr',...
+                  'RpIPSv','RpIPSh','Noise1L','Noise1L1','Noise1L2','Noise1L3',...
+                  'Noise1R','Noise1R1','Noise1R2','Noise1R3','Noise2L','Noise2L1',...
+                  'Noise2L2','Noise2L3','Noise2R','Noise2R1','Noise2R2','Noise2R3',...
+                  'Noise3L','Noise3L1','Noise3L2','Noise3L3','Noise4R','Noise4R1',...
+                  'Noise4R2','Noise4R3'};
+
     elseif strcmp(montage, 'ventral')
-        disp('asdf');
+        LIFGc  = EEG.data(1:3,   :);
+        RIFGc  = EEG.data(4:6,   :);
+        LMFGc  = EEG.data(7:9,   :);
+        RMFGc  = EEG.data(10:12, :);
+        LTPJc    = EEG.data(13:15, :);
+        RTPJc    = EEG.data(16:18, :);
+        LSTGc    = EEG.data(19:21, :);
+        RSTGc    = EEG.data(22:24, :);
+        NoiseLc    = EEG.data(25:27, :);
+        NoiseRc    = EEG.data(28:30, :);
+        NoiseFc    = EEG.data(31:33, :);
+        Noisec    = EEG.data(34:36, :);
+
+        LIFG = sqrt(sum(LIFGc.^2, 1));
+        RIFG = sqrt(sum(RIFGc.^2, 1));
+        LMFG = sqrt(sum(LMFGc.^2, 1));
+        RMFG = sqrt(sum(RMFGc.^2, 1));
+        LTPJ = sqrt(sum(LTPJc.^2, 1));
+        RTPJ = sqrt(sum(RTPJc.^2, 1));
+        LSTG = sqrt(sum(LSTGc.^2, 1));
+        RSTG = sqrt(sum(RSTGc.^2, 1));
+        NoiseL = sqrt(sum(NoiseLc.^2, 1));
+        NoiseR = sqrt(sum(NoiseRc.^2, 1));
+        NoiseF = sqrt(sum(NoiseFc.^2, 1));
+        Noise = sqrt(sum(Noisec.^2, 1));
+
+        EEG.data = [
+            LIFG  ; LIFGc;
+            RIFG  ; RIFGc;
+            LMFG  ; LMFGc;
+            RMFG  ; RMFGc;
+            LTPJ    ; LTPJc;
+            RTPJ    ; RTPJc;
+            LSTG    ; LSTGc;
+            RSTG    ; RSTGc;
+            NoiseL ; NoiseLc;
+            NoiseR ; NoiseRc;
+            NoiseF ; NoiseFc;
+            Noise ; Noisec;
+        ];
+
+        labels = {'LIFG','LIFGr','LIFGv','LIFGh','RIFG','RIFGr','RIFGv','RIFGh',...
+                  'LMFG','LMFGr','LMFGv','LMFGh','RMFG','RMFGr','RMFGv','RMFGh',...
+                  'LTPJ','LTPJr','LTPJv','LTPJh','RTPJ','RTPJr','RTPJv','RTPJh',...
+                  'LSTG','LSTGr','LSTGv','LSTGh','RSTG','RSTGr','RSTGv','RSTGh',...
+                  'NoiseL','NoiseL1','NoiseL2','NoiseL3','NoiseR','NoiseR1',...
+                  'NoiseR2','NoiseR3','NoiseF','NoiseF1','NoiseF2','NoiseF3',...
+                  'Noise','Noise1','Noise2','Noise3'};
+
     end
 
     EEG.nbchan = size(EEG.data, 1);
