9c06ec46f65269897e0329fe1e258230846d4e17
Jorge Yanar <jorgeyanar@gmail.com>
2017-02-18 18:57:41 -0600

    better docs for calculatePower.m

--- plugins/canlab1.0/calculatePower.m
+++ plugins/canlab1.0/calculatePower.m
@@ -1,8 +1,31 @@
-% Calculates absolute power between specified frequencies in the power spectrum
+% Calculates absolute power between specified frequencies in the power spectrum.
+%
+% Usage:
+%   >> power = calculatePower(PSD, frequencies, lowerFrequency, upperFrequency);
+%
+% Inputs:
+% PSD: A 1xN vector representing the power spectral density.
+%
+% frequencies: A 1xN vector representing the corresponding frequencies
+%              of the PSD.
+%
+% lowerFrequency: A scalar value specifying the lower frequency of the band
+%                 from which to calculate power.
+%
+% upperFrequency: A scalar value specifying the upper frequency of the band
+%                 from which to calculate power.
+%
+% For example, computing the absolute alpha power in a single channel is done
+% like so:
+% 
+%   >> [PSD, Freqs] = spectopo(EEG.data(1,:), 0, 512, 'plot', 'off');
+%   >> alphaPower = calculatePower(PSD, Freqs, 8, 13);
+%
+% Notes:
 % Spectopo returns the power spectrum density in units of 10*log10(uV^2 / Hz),
-% which is why the following transformation is necessary.[1][2]
+% which is why the 10.^ transformation is necessary.[1][2]
 % [1]: http://sccn.ucsd.edu/pipermail/eeglablist/2015/009249.html
 % [2]: http://sccn.ucsd.edu/pipermail/eeglablist/2015/009245.html
-function power = calculatePower(PSD, Spectra, lowerFrequency, higherFrequency)
-power = nanmean(10.^(PSD(Spectra >= lowerFrequency & Spectra <= higherFrequency)/10));
+function power = calculatePower(PSD, frequencies, lowerFrequency, upperFrequency)
+power = nanmean(10.^(PSD(frequencies >= lowerFrequency & frequencies <= upperFrequency)/10));
 end
--- plugins/canlab1.0/cl_preprocessingsrcmodels.m
+++ plugins/canlab1.0/cl_preprocessingsrcmodels.m
@@ -27,8 +27,8 @@
 % montage: String, specifies the source model we're preprocessing. Options:
 %                  'dmn': For preprocessing the Default Mode Network souce model.
 %                  'frontal': For preprocessing the frontal source model.
-%                  'ventral': For preprocessing the ventral source model.
 %                  'dorsal': For preprocessing the dorsal source model.
+%                  'ventral': For preprocessing the ventral source model.
 %
 % segments: Cell, specifies the events to extract from the EMSE .evt files. For
 %                 resting state, this is usually {'C', 'O'}.
@@ -41,27 +41,30 @@
 %                    Only supported option is currently 'CAR' for the common average.
 %
 
-importpath_mul =
-importpath_set =
-importpath_evt =
+importpath_mul_118 = '/Volumes/T3/118/rs/full/source-dorsal/clean-mul/';
+importpath_mul_127 = '/Volumes/T3/127/rs/full/source-dorsal/clean-mul/';
+importpath_set_118 = '/Volumes/T3/118/rs/full/original/raw-set/';
+importpath_set_127 = '/Volumes/T3/127/rs/full/original/raw-set/';
+importpath_evt = '/Users/jorge/Drive/research/_psd-slope/data/rs/full/evt/';
 
-exportpath_set =
-exportpath_mat =
+exportpath_set = '/Users/jorge/Drive/research/_psd-slope/data/rs/full/source-dorsal/MagEvtFiltCAR-set/';
+exportpath_mat = '/Users/jorge/Drive/research/_psd-slope/data/rs/full/source-dorsal/MagEvtFiltCAR-mat/';
 
-montage = 'frontal'
+montage = 'dorsal';
 segments = {'C', 'O'};
-filter_lofreq = 0.5
-filter_hifreq = 45
-reference = 'CAR'
+filter_lofreq = 0.5;
+filter_hifreq = 45;
+reference = 'CAR';
 
 % Import raw data; compute channel magnitudes from components
-cl_importmul(importpath_mul, importpath_set, exportpath_set);
+cl_importmul(importpath_mul_118, importpath_set_118, exportpath_set, montage);
+cl_importmul(importpath_mul_127, importpath_set_127, exportpath_set, montage);
 
 % Add event-related information to identify clean segments in data
 cl_modifyeventsEMSE(exportpath_set, importpath_evt, exportpath_set, segments);
 
 % Apply 0.5 - 45 Hz bandpass filter
-cl_bandpassfilter(exportpath_set, exportpath_set, filter_lofreq, filter_hifreq)
+cl_bandpassfilter(exportpath_set, exportpath_set, filter_lofreq, filter_hifreq);
 
 % Re-reference data to common average
 cl_rereference(exportpath_set, exportpath_set, reference);
